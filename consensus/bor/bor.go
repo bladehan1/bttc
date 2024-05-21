590e7575115dfb3982c575b8bfd1651b19c34b71
bladehan1 <bladehan@163.com>
2024-05-21 19:06:59 +0800

    add: separate err in verifyCascadingFields

--- consensus/bor/bor.go
+++ consensus/bor/bor.go
@@ -419,12 +419,14 @@ func (c *Bor) verifyCascadingFields(chain consensus.ChainHeaderReader, header *t
 	// Retrieve the snapshot needed to verify this header and cache it
 	snap, err := c.snapshot(chain, number-1, header.ParentHash, parents)
 	if err != nil {
+		log.Debug("error in  ", "err", err)
 		return err
 	}
 	// Verify the validator list match the local contract
 	if isSprintStart(number+1, c.config.Sprint) {
 		newValidators, err := c.GetCurrentValidatorsByBlockNrOrHash(context.Background(), rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber), number+1)
 		if err != nil {
+			log.Debug("error in  ", "err", err)
 			return err
 		}
 
@@ -433,6 +435,7 @@ func (c *Bor) verifyCascadingFields(chain consensus.ChainHeaderReader, header *t
 		headerVals, err := ParseValidators(header.Extra[extraVanity : len(header.Extra)-extraSeal])
 
 		if err != nil {
+			log.Debug("error in  ", "err", err)
 			return err
 		}
 
